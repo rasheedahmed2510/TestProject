0458b281a1e58e1d3b376fb58fb939b7770ff40f
Ahmed <inrahmed@ad.harman.com>
2019-09-26 11:16:47 +0530

    first commit

---
+++ src/test/java/com/qa/testproject/TestCaseTwo.java
@@ -0,0 +1,46 @@
+package com.qa.testproject;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestCaseTwo {
+	
+	@BeforeMethod
+	public void precondition(){
+		System.out.println("*************************");
+		System.out.println("Before Test");
+	}
+	
+	@Test
+	public void test1(){
+		System.out.println("Test case 1");
+	}
+	
+	
+	@Test
+	public void test2(){
+		System.out.println("Test case 2");
+	}
+	
+	@Test
+	public void test3(){
+		System.out.println("Test case 3");
+	}
+	
+	@Test
+	public void test4(){
+		System.out.println("Test case 4");
+	}
+	
+	@Test
+	public void test5(){
+		System.out.println("Test case 5");
+	}
+	
+	@AfterMethod
+	public void postondition(){
+		System.out.println("After Test");
+		System.out.println("*************************");
+	}
+}
